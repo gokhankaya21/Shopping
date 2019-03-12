ded0eeac5bc630f33abcab6ab895fc05a416337f
gokhankaya21 <semih_ahmet123@outlook.com>
2019-03-12 20:16:37 +0300

    Bir product için birden fazla image ekleyen komut yazıldı

--- DotNetShopping/DotNetShopping/DotNetShopping/Controllers/ProductController.cs
+++ DotNetShopping/DotNetShopping/DotNetShopping/Controllers/ProductController.cs
@@ -275,45 +275,6 @@ namespace DotNetShopping.Controllers
             db.SaveChanges();
             return RedirectToAction("Variants", new { id = variant.ProductId });
         }
-        public ActionResult PhotoAdd(Int64 id, Int64 ProductId)
-        {
-            ViewBag.Error = "";
-            var model = new PhotoAddModel();
-            model.VariantId = id;
-            model.ProductId = ProductId;
-            return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult PhotoAdd(PhotoAddModel model)
-        {
-            ViewBag.Error = "";
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    if (model.UploadedFile != null)
-                    {
-                        var image = System.Drawing.Image.FromStream(model.UploadedFile.InputStream);
-                        if (image.Width >= 1000 && image.Height >= 1000)
-                        {
-                            var productImage = new ProductImage();
-                            string fileName = productImage.InsertProductImage(model.VariantId);
-                            ImageHelper.SaveImage(fileName, image);
-                        }
-                        else
-                        {
-                            throw new Exception("Photo needs to be minimum 1000px X 1000px size");
-                        }
-                    }
-                }
-                return RedirectToAction("Variants", new { id = model.ProductId });
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Error = ex.Message;
-                return View(model);
-            }
-        }
+       
     }
 }
---
+++ DotNetShopping/DotNetShopping/DotNetShopping/Controllers/ProductImageController.cs
@@ -0,0 +1,61 @@
+﻿using DotNetShopping.Helpers;
+using DotNetShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetShopping.Controllers
+{
+    public class ProductImageController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: ProductImage
+        public ActionResult Index(Int64 id)
+        {
+            var images = db.ProductImages.Where(x => x.VariantId == id).OrderBy(x => x.Sequence);
+            return View(images.ToList());
+        }
+        public ActionResult PhotoAdd(Int64 id, Int64 ProductId)
+        {
+            ViewBag.Error = "";
+            var model = new PhotoAddModel();
+            model.VariantId = id;
+            model.ProductId = ProductId;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult PhotoAdd(PhotoAddModel model)
+        {
+            ViewBag.Error = "";
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (model.UploadedFile != null)
+                    {
+                        var image = System.Drawing.Image.FromStream(model.UploadedFile.InputStream);
+                        if (image.Width >= 1000 && image.Height >= 1000)
+                        {
+                            var productImage = new ProductImage();
+                            string fileName = productImage.InsertProductImage(model.VariantId);
+                            ImageHelper.SaveImage(fileName, image);
+                        }
+                        else
+                        {
+                            throw new Exception("Photo needs to be minimum 1000px X 1000px size");
+                        }
+                    }
+                }
+                return RedirectToAction("Index", new { id = model.VariantId });
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(model);
+            }
+        }
+    }
+}
