7ad5a56963ba76d98914cdc79a6ceedb65ff7e4a
gokhankaya21 <semih_ahmet123@outlook.com>
2019-03-14 17:59:50 +0300

    Tabloları sütunlara tıklayınca sıralayan komut yazıldı ve Userview'i oluşturuldu

---
+++ DotNetShopping/DotNetShopping/DotNetShopping/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using DotNetShopping.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetShopping.Controllers
+{
+    public class UserController : Controller
+    {
+        private IdentityDbContext db = new IdentityDbContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            var users = db.Users.Include("Roles").ToList();
+            return View(users);
+        }
+    }
+}
