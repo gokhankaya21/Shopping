5670f5e87cf5b40bbb80600317998ad264949d0d
gokhankaya21 <semih_ahmet123@outlook.com>
2019-03-15 17:58:45 +0300

    User rol ekleme,rolleri düzenleme ve rolü silme komutları eklendi

--- DotNetShopping/DotNetShopping/DotNetShopping/Models/UserModel.cs
+++ DotNetShopping/DotNetShopping/DotNetShopping/Models/UserModel.cs
@@ -11,4 +11,10 @@ namespace DotNetShopping.Models
         public string Email { get; set; }
         public string Roles { get; set; }
     }
+    public class UserRolesModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
 }
