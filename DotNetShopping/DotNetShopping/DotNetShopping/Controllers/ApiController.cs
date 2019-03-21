90974b0ec3a7568b5060e9c67122163f5ee9f2eb
gokhankaya21 <semih_ahmet123@outlook.com>
2019-03-21 18:37:11 +0300

    Product Detail sayfasına adet arttırma ve sepete ekleme komutları yazıldı

---
+++ DotNetShopping/DotNetShopping/DotNetShopping/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetShopping.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        [HttpPost]
+        public ActionResult AddToCart(Int64 VariantId, int Qty)
+        {
+            System.Threading.Thread.Sleep(1000);
+            var UserId = User.Identity.GetUserId();
+            return Json(new { Success = true });
+        }
+    }
+}
